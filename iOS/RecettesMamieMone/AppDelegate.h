7864bcd6f53446049dc54691ee6e56c224f2be73
Alexandre Thomas <alexandre.thomas@accedo.tv>
2017-02-15 17:13:42 +0100

    adding blank project for iOS

---
+++ iOS/RecettesMamieMone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecettesMamieMone
+//
+//  Created by Alexandre Thomas on 2/15/17.
+//  Copyright © 2017 MBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
