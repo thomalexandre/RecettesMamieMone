a2012105d3814e2e9bb258db8271422c71fefae7
Alexandre Thomas <alexandre.thomas@accedo.tv>
2017-02-15 22:34:03 +0100

    configuration manager

---
+++ iOS/RecettesMamieMone/Classes/Managers/ConfigurationManager.m
@@ -0,0 +1,26 @@
+//
+//  ConfigurationManager.m
+//  RecettesMamieMone
+//
+//  Created by Alexandre Thomas on 2/15/17.
+//  Copyright © 2017 MBC. All rights reserved.
+//
+
+#import "ConfigurationManager.h"
+
+@implementation ConfigurationManager
+
++ (instancetype)instance
+{
+    static ConfigurationManager *_instance = nil;
+    static dispatch_once_t oncePredicate;
+    
+    dispatch_once(&oncePredicate, ^
+                  {
+                      _instance = [[self alloc] init];
+                  });
+    
+    return _instance;
+}
+
+@end
